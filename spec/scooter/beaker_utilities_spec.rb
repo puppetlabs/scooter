6a4d8eccb8a9d0cde645b4622b7b7392c26008b0
Zach Reichert <zach.reichert@puppetlabs.com>
2015-03-31 16:29:41 -0700

    Tests for BeakerUtilities

---
+++ spec/scooter/beaker_utilities_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+module Scooter
+
+  describe Utilities::BeakerUtilities do
+
+    let(:result)          { instance_double(Beaker::Result)}
+    let(:master)          { instance_double(Beaker::Host) }
+
+    context 'with correct arguement' do
+
+      it 'gets the pe ca certfile' do
+        cmd = "cat `puppet agent --configprint localcacert`"
+        expect_any_instance_of(Beaker::DSL).to receive(:on).with(master, cmd).and_return(result)
+        expect(result).to receive(:stdout).and_return('foobar')
+        expect(subject.pe_ca_cert_file(master)).to match(/\/.*pe_certs.*\/cacert.pem/)
+      end
+
+      it 'gets the pe hostprivkey' do
+        cmd = "cat `puppet agent --configprint hostprivkey`"
+        expect_any_instance_of(Beaker::DSL).to receive(:on).with(master, cmd).and_return(result)
+        expect(result).to receive(:stdout).and_return('foobar')
+        expect(subject.pe_private_key(master)).to eq('foobar')
+      end
+
+      it 'gets the pe hostcert' do
+        cmd = "cat `puppet agent --configprint hostcert`"
+        expect_any_instance_of(Beaker::DSL).to receive(:on).with(master, cmd).and_return(result)
+        expect(result).to receive(:stdout).and_return('foobar')
+        expect(subject.pe_hostcert(master)).to eq('foobar')
+      end
+
+    end
+
+    context 'without correct arguements' do
+
+      it 'getting the pe ca certificate fails with no arguements' do
+        expect{ subject.pe_ca_cert_file }.to raise_error(ArgumentError)
+      end
+
+      it 'getting the pe hostprivkey fails with no arguements' do
+        expect{ subject.pe_private_key }.to raise_error(ArgumentError)
+      end
+
+      it 'getting the pe hostcert fails with no arguements' do
+        expect{ subject.pe_hostcert }.to raise_error(ArgumentError)
+      end
+
+    end
+
+  end
+
+end
