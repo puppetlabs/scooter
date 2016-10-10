4f54a17b211f6b32b8cfe73dcab7136fc648e057
Zach Reichert <zach.reichert@puppetlabs.com>
2016-10-10 11:26:43 -0700

    (MAINT) abstract orchestrator verison into module initialize

--- lib/scooter/httpdispatchers/orchestrator/v1/v1.rb
+++ lib/scooter/httpdispatchers/orchestrator/v1/v1.rb
@@ -4,70 +4,75 @@ module Scooter
       # Methods here are generally representative of endpoints
       module V1
 
+        def initialize(host)
+          super(host)
+          @version = 'v1'
+        end
+
         #jobs endpoints
         def get_last_jobs(n_jobs)
-          @connection.get("v1/jobs") do |req|
+          @connection.get("#{@version}/jobs") do |req|
             req.body = {:limit => n_jobs} if n_jobs
           end
         end
 
         def get_job(job_id)
-          @connection.get("v1/jobs/#{job_id}")
+          @connection.get("#{@version}/jobs/#{job_id}")
         end
 
         def get_nodes(job_id)
-          @connection.get("v1/jobs/#{job_id}/nodes")
+          @connection.get("#{@version}/jobs/#{job_id}/nodes")
         end
 
         def get_report(job_id)
-          @connection.get("v1/jobs/#{job_id}/report")
+          @connection.get("#{@version}/jobs/#{job_id}/report")
         end
 
         def get_events(job_id)
-          @connection.get("v1/jobs/#{job_id}/events")
+          @connection.get("#{@version}/jobs/#{job_id}/events")
         end
 
         #environments endpoints
         def get_environment(environment)
-          @connection.get("v1/environments/#{environment}")
+          @connection.get("#{@version}/environments/#{environment}")
         end
 
         def get_applications_in_environment(environment)
-          @connection.get("v1/environments/#{environment}/applications")
+          @connection.get("#{@version}/environments/#{environment}/applications")
         end
 
         def get_instances_in_environment(environment)
-          @connection.get("v1/environments/#{environment}/instances")
+          @connection.get("#{@version}/environments/#{environment}/instances")
         end
 
         #command endpoints
         def post_deploy(payload)
-          @connection.post("v1/command/deploy") do |req|
+          @connection.post("#{@version}/command/deploy") do |req|
             req.body = payload
           end
         end
 
         def post_stop(payload)
-          @connection.post("v1/command/stop") do |req|
+          @connection.post("#{@version}/command/stop") do |req|
             req.body = payload
           end
         end
 
         def post_plan(payload)
-          @connection.post("v1/command/plan") do |req|
+          @connection.post("#{@version}/command/plan") do |req|
             req.body = payload
           end
         end
 
         #inventory endpoints
         def get_inventory(node=nil)
-          url = "v1/inventory"
+          url = "#{@version}/inventory"
           url << "/#{node}" if node
           @connection.get(url)
         end
 
         def post_inventory(payload)
-          @connection.post('v1/inventory') do |req|
+          @connection.post("#{@version}/inventory") do |req|
             req.body = payload
           end
         end
